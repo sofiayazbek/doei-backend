8bbdc366c31e1a837be2dae09cee4b1704575ba3
SOFIA AINHOA <88836758+sofiayazbek@users.noreply.github.com>
2021-11-18 11:57:22 +0000

    login google

--- mysite/settings.py
+++ mysite/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+#    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -130,3 +130,13 @@ CORS_ALLOWED_ORIGINS = [
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+      'rest_framework.authentication.SessionAuthentication'
+    ],
+#    'DEFAULT_PERMISSION_CLASSES': [
+#      'rest_framework.permissions.IsAuthenticated'
+#    ]    
+}
--- todo/api.py
+++ todo/api.py
@@ -1,7 +1,17 @@
+from rest_framework import routers, serializers, viewsets, mixins
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+from rest_framework.viewsets import ViewSet
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from rest_framework.permissions import IsAuthenticated
 
+#from django.contrib.auth import authenticate, user_logged_in
+from django.contrib.auth import authenticate, login, logout
 from todo.models import Duvida, Instituicao, Produto, Doador, Contato, Assunto
 
-from rest_framework import routers, serializers, viewsets, mixins
 
 # Serializers define the API representation.
 class DuvidaSerializer(serializers.ModelSerializer):
@@ -70,6 +80,69 @@ class CreateContatoViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
   queryset = Contato.objects.all()
 
 
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = [
+            'id',
+            'username',
+            'password',
+        ]
+
+    @transaction.atomic
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        instance = self.Meta.model(**validated_data)
+        instance.set_password(password)
+        instance.save()
+        return instance
+
+class UserRegistrationViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+  serializer_class = UserRegistrationSerializer  
+
+
+class LoginViewSet(ViewSet):
+  @staticmethod
+  def create(request: Request) -> Response:
+      user = authenticate(
+          username=request.data.get('username'),
+          password=request.data.get('password'))
+
+      if user is not None:
+        login(request, user)
+        return JsonResponse({"detail": "Success"})
+      else:
+        return JsonResponse(
+            {"detail": "Invalid credentials"},
+            status=status.HTTP_401_UNAUTHORIZED,
+        )
+
+class UserDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username')
+
+class UserDetailsViewSet(ViewSet):
+  serializer_class = UserDetailsSerializer
+  permission_classes = [IsAuthenticated]
+  @staticmethod
+  def list(request: Request) -> Response:
+    content = {'not_done': 1}
+    print(request.user)
+    print(content)
+    serializer = UserDetailsSerializer(request.user, many=False)
+    #return 
+    return Response(serializer.data)
+
+
+class LogoutViewSet(ViewSet):
+  permission_classes = [IsAuthenticated]
+  @staticmethod
+  def list(request: Request) -> Response:
+    logout(request)
+    content = {'logout': 1}
+    return Response(content)    
+
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'duvidas', DuvidaViewSet)
@@ -77,5 +150,8 @@ router.register(r'instituicoes', InstituicaoViewSet)
 router.register(r'doadores-create', CreateDoadorViewSet)
 router.register(r'contatos-create', CreateContatoViewSet)
 router.register(r'assuntos', AssuntoViewSet)
-
+router.register(r'currentuser', UserDetailsViewSet, basename="Currentuser")
+router.register(r'login', LoginViewSet, basename="Login")
+router.register(r'logout', LogoutViewSet, basename="Logout")
+router.register(r'user-registration', UserRegistrationViewSet, basename="User")
   
